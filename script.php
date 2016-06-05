64e68693d5b01d90cc4c6ae3948b1e65fe172cd7
amrfayad <amr.fci2007@gmail.com>
2016-06-05 11:20:44 +0200

    intailize Project

---
+++ script.php
@@ -0,0 +1,22 @@
+<?php
+# Try running this locally.
+# Include the Autoloader (see "Libraries" for install instructions)
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+# Instantiate the client.
+$mgClient = new Mailgun('key-3ax6xnjp29jd6fds4gc373sgvjxteol0');
+$domain = "samples.mailgun.org";
+$to = filter_input(INPUT_POST, 'email');
+$subject = filter_input(INPUT_POST, 'subject');
+$content = filter_input(INPUT_POST, 'content');
+# Make the call to the client.
+$result = $mgClient->sendMessage("$domain",
+ array('from'    => 'Site Admin <admin@samples.mailgun.org>',
+        'to'      => 'Amr Fayad <'.$to.'>',
+        'subject' => $subject,
+        'text'    => $content));
+var_dump($result);
+
+
+?>
